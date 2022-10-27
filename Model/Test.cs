f2f60f359d9d63816ea30ffd976835e6577a2180
Marco <marcoiannuzzi@live.com.ar>
2022-10-27 20:01:40 -0300

    Agregar archivos de proyecto.

---
+++ Model/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetMentor.Model
+{
+    public class Test
+    {
+        public static int Multiplicar ( int x)
+        {
+            return x * x;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using NetMentor.Model;
+
+for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine("ingresa un numero");
+    var num = int.Parse(Console.ReadLine());
+
+    if (num % 2 == 0)
+    {
+        Console.WriteLine($"{num} es par");
+        var x = Test.Multiplicar(num);
+        Console.WriteLine($"num x num es {x}");
+    }
+
+}
